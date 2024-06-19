04ef9c071f161efb81dd63749887b97fbcf9a626
Drilack7 <drilack7@gmail.com>
2024-06-19 14:00:10 +0300

    websockets, readme and final dockerfile

---
+++ README.md
@@ -0,0 +1,40 @@
+# Test Task QA / SDET TRADING
+
+This repository builds an automated test suite for a `RESTful API` that simulates a trading platform with WebSocket support in `Docker containers`.
+
+The API and the Testsuite are developed in `Python` and `Pytest`
+
+## Steps to run the tests in Docker
+
+**Prerequisites**:
+
+- Docker
+
+**Steps**:
+
+**For the API server**:
+
+- Build the Docker image:
+    - `docker build -t server_img -f Dockerfile.server .`
+- Create the container:
+    - `docker run -d --name server_container -p 8000:8000 server_img`
+- Check the server endpoints and documentation [here](http://localhost:8000/docs)
+
+**For the Testsuite**:
+
+- Build the Docker image:
+    - `docker build -t test_img -f Dockerfile.test .`
+- Create the container:
+    - `docker run -it --name test_container test_img`
+- Copy out the test report:
+    - `docker cp test_container:/code/report.html ./new_report.html`
+
+**Cleanup**:
+- Stop the running server container:
+    - `docker stop server_container`
+- Delete the containers:
+    - `docker rm -f server_container`
+    - `docker rm -f test_container`
+- Delete the images:
+    - `docker rmi server_img:latest`
+    - `docker rmi test_img:latest`
--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, WebSocket
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import random
-import time
+import asyncio
+import json
+
+app = FastAPI(
+    openapi_version="3.0.0",
+    title="Forex Trading Platform API",
+    description="A RESTful API to simulate a Forex trading platform with WebSocket support for real-time order updates.",
+    version="1.0.0",
+    servers=[{'url': 'http://localhost:8000'}]
+)
 
 orders = []  #database
 
@@ -14,30 +24,22 @@ class OrderOutput(BaseModel):
     stock: str
     quantity: int
 
-app = FastAPI(
-    openapi_version="3.0.0",
-    title="Forex Trading Platform API",
-    description="A RESTful API to simulate a Forex trading platform with WebSocket support for real-time order updates.",
-    version="1.0.0",
-    servers=[{'url': 'http://localhost:8000'}]
-)
-
 @app.get("/orders/", response_description="A list of orders")
 async def retrieve_all_orders():
-    time.sleep(random.uniform(0, 1))
+    await asyncio.sleep(random.uniform(0, 1))
     return orders
 
 @app.post("/orders/", response_description="Order placed", status_code=status.HTTP_201_CREATED)
 async def place_a_new_order(order: OrderInput):
-    time.sleep(random.uniform(0, 1))
+    await asyncio.sleep(random.uniform(0, 1))
     new_order_id = len(orders)+1
-    order_output = OrderOutput(id=new_order_id, stock=order.stock, quantity=order.quantity)
+    order_output = OrderOutput(id=new_order_id, stock=order.stock, quantity=order.quantity)  # convert input foramt to output
     orders.append(order_output)
     return {"msg": f"Order placed successfully with id={new_order_id}"}
 
 @app.get("/orders/{order_id}", response_description="Order found")
 async def retrieve_a_specific_order(order_id: int):
-    time.sleep(random.uniform(0, 1))
+    await asyncio.sleep(random.uniform(0, 1))
     match = [order for order in orders if order_id == order.id]
     if not match:
         raise HTTPException(status_code=404, detail="Order not found")
@@ -46,7 +48,7 @@ async def retrieve_a_specific_order(order_id: int):
 
 @app.delete("/orders/{order_id}", response_description="Order canceled", status_code=204)
 async def cancel_an_order(order_id: int):
-    time.sleep(random.uniform(0, 1))
+    await asyncio.sleep(random.uniform(0, 1))
     match = [order for order in orders if order_id == order.id]
     if not match:
         raise HTTPException(status_code=404, detail="Order not found")
@@ -54,5 +56,29 @@ async def cancel_an_order(order_id: int):
         index = orders.index(match[0])
         deleted_order = orders.pop(index)
         return deleted_order
+
+#######################################################################################################################
+############################################## Websocket ##############################################################
+#######################################################################################################################
+
+with open('app/websocket_page.html', 'r') as f:
+    html = f.read()
+
+@app.get("/ws")
+async def WebSocket_connection_for_real_time_order_information():
+    return HTMLResponse(html)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        order_ws = await websocket.receive_text()
+        order = json.loads(order_ws)  # converts json to dict
+        new_order_id = len(orders) + 1
+        order_output = OrderOutput(id=new_order_id, stock=order["stock"], quantity=order["quantity"])
+        orders.append(order_output)
+        await websocket.send_text(f"Order with id={new_order_id} is Pending...")
+        await asyncio.sleep(random.uniform(0, 3))
+        await websocket.send_text(f"Order with id={new_order_id} was Executed--->" + str(order_output))
 
 #fastapi dev app/main.py
